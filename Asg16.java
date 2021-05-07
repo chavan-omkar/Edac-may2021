174bfd8262d9eef2ddccd0d8995b9b69b8c1f14e
omkarchavan-JH <83566321+omkarchavan-JH@users.noreply.github.com>
2021-05-08 01:45:18 +0530

    Add files via upload

---
+++ Asg16.java
@@ -0,0 +1,15 @@
+class Asg16
+{
+ public static void main(String args[])
+{
+for(int i=1; i<=5;i++)
+{int num=5;
+for(int j=1; j<=i;j++)
+{
+System.out.print(num+" ");
+num--;
+}
+System.out.println();
+}
+}
+}
